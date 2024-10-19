c797587a99116ae37349568c2b800887ad2713e4
mio <180114449+qubicmio@users.noreply.github.com>
2024-10-19 17:52:54 +0000

    Change root url default

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const ROOT_URL_QX_SERVICE= 'https://dev.qubic.at:8443/service/v1/qx'
+export const ROOT_URL_QX_SERVICE= 'https://dev.qubic.at:8443/api/service/v1/qx'
 // export const ROOT_URL_QX_SERVICE= 'http://localhost:8081/service/v1/qx'
